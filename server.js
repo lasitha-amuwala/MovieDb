19d8ef7db9f11582b25c7e1f21c6318776f2c254
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-01-10 17:27:09 -0500

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const userData = require("./data/users.json");
 

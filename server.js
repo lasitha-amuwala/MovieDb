431e9e705ae4a0357d5dd6ad483645b9542c9ef6
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-01-10 22:06:13 -0500

    updated browse movie page design

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv/config')
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
